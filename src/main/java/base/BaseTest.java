18d0defcdc16b625a92a3ce306f69c54e55cede0
vinit <vinitkkulkarni@gmail.com>
2026-01-15 22:05:21 +0530

    Fix compilation: remove WebDriverManager import

--- src/main/java/base/BaseTest.java
+++ src/main/java/base/BaseTest.java
@@ -6,7 +6,6 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BaseTest {
 	public WebDriver driver;
