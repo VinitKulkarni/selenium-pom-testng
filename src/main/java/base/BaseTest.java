a06a4808604348cda95aee2af7c9b26024aff39b
vinit <vinitkkulkarni@gmail.com>
2026-01-15 21:46:22 +0530

    Fix: run chrome in headless mode for GitHub Actions

--- src/main/java/base/BaseTest.java
+++ src/main/java/base/BaseTest.java
@@ -2,14 +2,25 @@ package base;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BaseTest {
 	public WebDriver driver;
 
     @BeforeMethod
     public void setup() {
+    	WebDriverManager.chromedriver().setup();
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+
+        driver = new ChromeDriver(options);
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://www.saucedemo.com/");
